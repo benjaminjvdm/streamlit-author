f9116ea8fff097dba3bae810ec6f816355b8f9d4
Benjamin (James) van der Merwe <97449931+benjaminjvdm@users.noreply.github.com>
2025-03-15 23:06:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-st.subheader("About the Author")
+st.sidebar.subheader("About the Author")
 
-    image_url = "https://github.com/benjaminjvdm/streamlit-author/blob/main/Untitled%20design(1)(1).png?raw=true"
-    try:
-        response = requests.get(image_url)
-        response.raise_for_status()
-        image = response.content
-        st.image(image, caption="Moon Benjee (문벤지)", use_container_width=True)
-    except requests.exceptions.RequestException as e:
-        st.error(f"Error loading image: {e}")
+image_url = "https://github.com/benjaminjvdm/streamlit-author/blob/main/Untitled%20design(1)(1).png?raw=true"
+try:
+    response = requests.get(image_url)
+    print(f"Image URL: {image_url}")
+    response.raise_for_status()
+    image = response.content
+    st.sidebar.image(image, caption="Moon Benjee (문벤지)")
+except requests.exceptions.RequestException as e:
+    st.sidebar.error(f"Error loading image: {e}")
 
-    st.markdown(
-        """
-        This app was Built with ❤️ by **Benjee(문벤지)**. 
-        You can connect with me on: [LinkedIn](https://www.linkedin.com/in/benjaminjvdm/)
-        """
-    )
+st.sidebar.markdown(
+    """
+    This app was Built with ❤️ by **Benjee(문벤지)**.
+    You can connect with me on: [LinkedIn](https://www.linkedin.com/in/benjaminjvdm/)
+    """
+)
